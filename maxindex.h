7fd2f274d556785f79387f1a48ae0b9133c904ea
bluepp <bluepp@gmail.com>
2014-05-28 15:54:46 +0800

    Create maxindex.h

---
+++ maxindex.h
@@ -0,0 +1,8 @@
+/*
+一个数组，找最大数，可能有重复，要求random输出最大index，
+比如[ 1 2 3 4 5 6 6 6], 最大的是6， index可能是5，6， 7。 每次call这个
+function的时候，random输出5，6，7. 
+
+source: http://www.mitbbs.com/article_t/JobHunting/32701375.html
+Facebook tel
+*/
