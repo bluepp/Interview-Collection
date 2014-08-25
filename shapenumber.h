53652a53048946dd67f5f0b8ad41115a1c9fd3d4
bluepp <bluepp@gmail.com>
2014-08-25 11:52:50 +0800

    Create shapenumber.h

---
+++ shapenumber.h
@@ -0,0 +1,9 @@
+/*
+  2014-08-25
+  bluepp
+  May the force be with me!
+
+
+  Give a 2D integerer matrix, calaculate the number of shapes. 
+  Shapes is defined as the parts which are composed of same
+*/  
