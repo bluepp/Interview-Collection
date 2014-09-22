ef6eb8d80ecd7e0e66fd7eb92891115f3be39132
bluepp <bluepp@gmail.com>
2014-09-22 17:02:06 +0800

    Create UpsideDownBinaryTree.h

---
+++ UpsideDownBinaryTree.h
@@ -0,0 +1,15 @@
+/*
+  2014-09-22
+  bluepp
+  May the force be with me
+
+http://www.mitbbs.com/article_t1/JobHunting/32775405_0_1.html
+
+Given a binary tree where all the right nodes are leaf nodes, flip it 
+upside down
+and turn it into a tree with left leaf nodes.
+
+
+https://github.com/yxjiang/algorithms/blob/master/src/main/java/algorithm/linkedin/UpsideDownBinaryTree.java
+
+*/
