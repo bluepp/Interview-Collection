5b80a44835404c8d620d8417ff8c23655217a56b
bluepp <bluepp@gmail.com>
2014-11-04 13:26:26 +0800

    Create facebook.h

---
+++ facebook.h
@@ -0,0 +1,12 @@
+2014-11-04
+
+http://www.mitbbs.com/article_t1/JobHunting/32817599_0_1.html
+
+忘了分享面试题目了
+
+1. Manager聊经历半小时 然后半小时coding: 给你一个read_buffer(char*buffer, 
+int len)，用这个实现read_line()，完全设计这个interface.
+2. 给个数组，找出所有的三个数字trio, 加起来等于目标数字
+3. 设计tinyurl
+4. 给一个dict，然后一个长字符串，和长度len，找出所有长字符串里长度为len的在
+字典内的子串。
