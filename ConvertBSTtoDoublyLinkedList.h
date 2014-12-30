01b310de354d116c7015cc39c824fa8828dd2f33
bluepp <bluepp@gmail.com>
2014-12-30 16:36:19 +0800

    Update ConvertBSTtoDoublyLinkedList.h

--- ConvertBSTtoDoublyLinkedList.h
+++ ConvertBSTtoDoublyLinkedList.h
@@ -6,6 +6,42 @@
   Convert Binary Search Tree (BST) to Sorted Doubly-Linked List
 */
 
+
+/* v2, my version */
+//convert binary search tree to sorted doubly linked list
+
+Node *TreeToList(Node *root)
+{
+    Node *head = NULL;
+    
+    _convert(root, NULL, head);
+    return head;
+}
+
+void _convert(Node *p, Node *&prev, Node *&head)
+{
+    if (!p) return;
+    
+    _convert(p->left, prev, head);
+    
+    if (prev)
+        p->left = prev;
+    else
+        head = p;
+    
+    Node *right = p->right;
+    if (head) 
+    {
+        head->left = p;
+        p->right = head;
+    }
+    
+    prev = p;
+    
+    _convert(right, prev, head);
+}
+
+
 /* http://leetcode.com/2010/11/convert-binary-search-tree-bst-to.html */
 //convert binary search tree to sorted doubly linked list
 
